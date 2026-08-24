59484246bd7a4f19f1089d7b8cb25d797a0bd62c
GUNTIPALLY TEJA SRI <128918310+tejasrichowdary26@users.noreply.github.com>
2026-08-25 04:04:48 +0530

    Bulk sync: 2 submissions

--- Data Structures & Algorithms/is-anagram/submission-0.java
+++ Data Structures & Algorithms/is-anagram/submission-0.java
@@ -1,14 +1,23 @@
 class Solution {
     public boolean isAnagram(String s, String t) {
-     if(s.length()!=t.length()){
-      return false;
-     }
-    char[] sSort = s.toCharArray();
-    char[] tsort = t.toCharArray();
-    Arrays.sort(sSort);
-    Arrays.sort(tsort);
-    return Arrays.equals(sSort,tsort);
-
-}
-
+        if(s.length()!=t.length()){
+            return false;
+        }
+        HashMap<Character,Integer> val=new HashMap<>();
+        for(int i=0;i<s.length();i++){
+            char ch =s.charAt(i);
+            val.put(ch,val.getOrDefault(ch,0)+1);
+        }
+        for(int i=0;i<t.length();i++){
+            char ch =t.charAt(i);
+            if(!val.containsKey(ch)){
+                return false;
+            }
+            val.put(ch,val.get(ch)-1);
+            if(val.get(ch)==0){
+                val.remove(ch);
+            }
+        }
+        return val.isEmpty();
+    }
 }
